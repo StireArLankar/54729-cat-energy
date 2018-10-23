8bb2c53696e82d94821933fe02adc15891ffbc50
StireArLankar <stire.ru@gmail.com>
2018-10-23 19:54:13 +0300

    Исправление на querySelector

--- source/js/programm-script.js
+++ source/js/programm-script.js
@@ -1,19 +1,19 @@
 var form = document.querySelector(".programm-form");
 var array = [
   {
-    element: document.getElementById("cat-name-field"),
+    element: document.querySelector("#cat-name-field"),
     reg: /\w+/
   },
   {
-    element: document.getElementById("cat-weight-field"),
+    element: document.querySelector("#cat-weight-field"),
     reg: /[0-9]/
   },
   {
-    element: document.getElementById("owner-email-field"),
+    element: document.querySelector("#owner-email-field"),
     reg: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
   },
   {
-    element: document.getElementById("owner-phone-field"),
+    element: document.querySelector("#owner-phone-field"),
     reg: /8\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}/
   }
 ];
